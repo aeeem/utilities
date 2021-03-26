e8eda4beb25655aca54e14e0f7865b369e5cc25e
arif maulana <arifmaulanaa@gmail.com>
2021-03-26 20:22:10 +0700

    Add error standard

--- helper.go
+++ helper.go
@@ -2,6 +2,7 @@ package utilities
 
 import (
 	"encoding/base64"
+	"net/http"
 	"time"
 )
 
@@ -30,4 +31,69 @@ func EncodeCursor(t time.Time) string {
 	timeString := t.Format(timeFormat)
 
 	return base64.StdEncoding.EncodeToString([]byte(timeString))
+}
+
+// ResponseError represent the reseponse error struct
+type ResponseError struct {
+	Message string      `json:"message"`
+	Data    interface{} `json:"data"`
+}
+
+type ResponseList struct {
+	Message    string      `json:"message"`
+	Data       interface{} `json:"data"`
+	NextCursor string      `json:"next_cursor"`
+}
+
+type ResponseStandard struct {
+	Message    string      `json:"message"`
+	Data       interface{} `json:"data"`
+	NextCursor string      `json:"next_cursor"`
+}
+
+func StandardResponse(input interface{}, message string) (statusCode int64, output interface{}) {
+	output = ResponseStandard{
+		Data:    input,
+		Message: "Ok",
+	}
+	statusCode = http.StatusOK
+	return
+}
+
+func ErrorResponse(err error) (statusCode int64, output interface{}) {
+	output = ResponseError{
+		Message: err.Error(),
+	}
+	statusCode = int64(getStatusCode(err))
+	return
+}
+
+func ListResponse(input interface{}, nextCursor string, message string) (statusCode int64, output interface{}) {
+	output = ResponseList{
+		Data:       input,
+		NextCursor: nextCursor,
+		Message:    message,
+	}
+	return
+}
+
+func getStatusCode(err error) int {
+	if err == nil {
+		return http.StatusOK
+	}
+
+	switch err {
+	case ErrInternalServerError:
+		return http.StatusInternalServerError
+	case ErrNotFound:
+		return http.StatusNotFound
+	case ErrConflict:
+		return http.StatusConflict
+	case ErrBadParamInput:
+		return http.StatusBadRequest
+	case ErrForbiden:
+		return http.StatusForbidden
+	default:
+		return http.StatusInternalServerError
+	}
 }
