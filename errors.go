2107919fb11d3e5a182a494a96e226b70678cbc7
arif maulana <arifmaulanaa@gmail.com>
2021-12-22 19:00:09 +0700

    adding new error types

--- errors.go
+++ errors.go
@@ -13,10 +13,12 @@ var (
 	ErrBadParamInput = errors.New("Given Param is not valid")
 	// ErrUnauthorized will throw if the given request-body or params is not valid
 	ErrUnauthorized = errors.New("Unauthorize")
-	ErrForbiden     = errors.New("The Resource Is Forbidden")
+	// Errforbiden will throw if user with the given role does not have permission to access the resource
+	ErrForbiden = errors.New("The Resource Is Forbidden")
 	// ErrDuplicateLogin  will throw if the given request-body or params is not valid
 	ErrDuplicateLogin = errors.New("User Already Login, if its not you please change your password.")
-
-	// ErrDuplicateLogin  will throw if the given request-body or params is not valid
+	// ErrDuplicateEmail  will throw if the given request-body or params is not valid
 	ErrDuplicateEmail = errors.New("User Email Already Exist. Please Change Your email")
+	//ErrNeedUpdate will throw if the current apps need to be updated
+	ErrNeedUpdate = errors.New("Please update your apps")
 )
